c5a9b8b4d963ae618ed4d5f45b5a9d6a0af782b6
adilrazaa <adilraza22262@gmail.com>
2026-08-08 10:42:52 +0530

    Optimized leetcode 852

--- Demo.java
+++ Demo.java
@@ -6,14 +6,17 @@ class Demo{
         System.out.println(p.peakIndexInMountainArray(arr));
     }
     public int peakIndexInMountainArray(int[] arr){
-        int max=arr[0];
-        int maxIndex=0;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
-                maxIndex=i;
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
             }
         }
-        return maxIndex;
+        return start;
     }
 }
